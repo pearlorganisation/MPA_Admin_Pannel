12b5f36d5d51d79840641a4632868afd478dabd6
pearlorganisation <data-backup@pearlorganisation.com>
2026-02-16 16:40:15 +0530

    changes

--- src/services/baseApi.js
+++ src/services/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4040/api/v1",
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
 
